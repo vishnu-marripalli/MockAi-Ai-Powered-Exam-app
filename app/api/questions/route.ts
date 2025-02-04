75441f225306559f493a2ab3ebc7323c7da301e6
vishnu-marripalli <marripallivishnuvardhan@gmail.com>
2025-02-04 19:42:05 +0530

    f

--- app/api/questions/route.ts
+++ app/api/questions/route.ts
@@ -1,20 +1,14 @@
-
-
-export const dynamic = 'force-static'
-
+export const config = { runtime: "edge" };
 
 import { NextResponse } from "next/server";
 import { fetchQuestions } from "@/lib/aiClient";
 
 export async function POST(req: Request) {
-    // "use server";  // Ensure it's treated as a server function
-
-  const { topic } = await req.json(); // Get topic from frontend/
-  try {
-    const questions = await fetchQuestions(topic);
-    // console.log(questions);
-    return NextResponse.json({ success: true, data: questions });
-  } catch (error) {
-    return NextResponse.json({ success: false, error: "Failed to fetch questions" });
-  }
+    try {
+        const { topic } = await req.json();
+        const questions = await fetchQuestions(topic);
+        return NextResponse.json({ success: true, data: questions });
+    } catch (error) {
+        return NextResponse.json({ success: false, error: "Failed to fetch questions" });
+    }
 }
